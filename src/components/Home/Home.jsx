7fc0de7e68612e175c1ec18bad49c7e803aa1551
unknown <75127793+propiesek123@users.noreply.github.com>
2023-01-29 18:58:00 +0100

    added title at homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,8 @@ import Search from "../Search/Search";
 
 const Home = () => {
   return (
-    <div className="h-screen w-screen flex items-center justify-around flex-col">
+    <div className="w-screen flex items-center justify-around flex-col gap-10">
+      <h1 className="text-emerald-400 text-3xl">lichess.org Analyser</h1>
       <Search />
     </div>
   );
